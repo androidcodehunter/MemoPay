da763cd63fcb0123a70cce6a9123ada82937e5f2
Sharif <sharifur101@gmail.com>
2020-03-01 11:04:57 +0600

    initial fragment added.

--- app/src/main/java/com/memo/pay/ui/home/HomeFragment.kt
+++ app/src/main/java/com/memo/pay/ui/home/HomeFragment.kt
@@ -8,16 +8,10 @@ import android.view.ViewGroup
 
 import com.memo.pay.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
--- app/src/main/java/com/memo/pay/ui/onboarding/SplashActivity.kt
+++ app/src/main/java/com/memo/pay/ui/onboarding/SplashActivity.kt
@@ -2,11 +2,28 @@ package com.memo.pay.ui.onboarding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.memo.pay.ui.home.MainActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        IntroActivity.startIntroActivity(this)
+        launchOnboardingAfterOneSeconds()
     }
+
+    private fun launchOnboardingAfterOneSeconds() {
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(DELAY_ONE_SECONDS)
+            MainActivity.startMainActivity(this@SplashActivity)
+        }
+    }
+
+    companion object{
+        const val DELAY_ONE_SECONDS = 1000L
+    }
+
 }
