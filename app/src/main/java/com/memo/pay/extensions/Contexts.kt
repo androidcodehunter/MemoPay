8a7cc8bfe062faea0e32c153642c196bbc83adbf
Sharif <sharifur101@gmail.com>
2020-03-05 08:25:12 +0600

    add internet checker.

--- app/src/main/java/com/memo/pay/extensions/Contexts.kt
+++ app/src/main/java/com/memo/pay/extensions/Contexts.kt
@@ -1,9 +1,12 @@
 package com.memo.pay.extensions
 
+import android.Manifest
 import android.app.Activity
 import android.content.Context
+import android.net.ConnectivityManager
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.annotation.RequiresPermission
 import androidx.fragment.app.Fragment
 
 
@@ -18,5 +21,14 @@ fun Activity.hideKeyboard(){
 fun Context.hideKeyboard(view: View){
     val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+}
 
+@RequiresPermission(value = Manifest.permission.ACCESS_NETWORK_STATE)
+fun Context.isConnected(): Boolean {
+    val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    connectivityManager?.let {
+        val networkInfo = it.activeNetworkInfo
+        networkInfo?.let { info -> if (info.isConnected) return true }
+    }
+    return false
 }
