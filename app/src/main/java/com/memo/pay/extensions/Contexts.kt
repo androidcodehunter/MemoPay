a4e04ae49c35e821377d8b95e29e86acd47178e6
Sharif <sharifur101@gmail.com>
2020-03-05 20:04:00 +0600

    add internet checker.

--- app/src/main/java/com/memo/pay/extensions/Contexts.kt
+++ app/src/main/java/com/memo/pay/extensions/Contexts.kt
@@ -4,6 +4,8 @@ import android.Manifest
 import android.app.Activity
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.annotation.RequiresPermission
@@ -26,9 +28,26 @@ fun Context.hideKeyboard(view: View){
 @RequiresPermission(value = Manifest.permission.ACCESS_NETWORK_STATE)
 fun Context.isConnected(): Boolean {
     val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-    connectivityManager?.let {
-        val networkInfo = it.activeNetworkInfo
-        networkInfo?.let { info -> if (info.isConnected) return true }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+        val networkCapabilities = connectivityManager.activeNetwork ?: return false
+        val actNw = connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+        return when {
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
+    }else{
+        connectivityManager.run {
+            connectivityManager.activeNetworkInfo?.run {
+                return when (type) {
+                    ConnectivityManager.TYPE_WIFI -> true
+                    ConnectivityManager.TYPE_MOBILE -> true
+                    ConnectivityManager.TYPE_ETHERNET -> true
+                    else -> false
+                }
+            }
+        }
     }
     return false
 }
