187501a19e58f7f8681e23e1af96e69e315a8037
Sharif <sharifur101@gmail.com>
2020-03-06 09:40:04 +0600

    add initial confirmtransfer fragment.

--- app/src/main/java/com/memo/pay/ui/sendmoney/ContactSelectionFragment.kt
+++ app/src/main/java/com/memo/pay/ui/sendmoney/ContactSelectionFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.memo.pay.R
+import kotlinx.android.synthetic.main.fragment_contact_selection.*
 
 class ContactSelectionFragment: Fragment() {
 
@@ -15,5 +17,13 @@ class ContactSelectionFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_contact_selection, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val vavController = Navigation.findNavController(view)
+        btnSendViaOtp.setOnClickListener {
+            vavController.navigate(R.id.action_contactSelectionFragment_to_confirmTransferFragment)
+        }
     }
 }
--- app/src/main/java/com/memo/pay/ui/sendmoney/SendMoneyFragment.kt
+++ app/src/main/java/com/memo/pay/ui/sendmoney/SendMoneyFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.memo.pay.R
+import kotlinx.android.synthetic.main.fragment_send_money.*
 
 class SendMoneyFragment: Fragment() {
 
@@ -19,6 +21,10 @@ class SendMoneyFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val vavController = Navigation.findNavController(view)
+        btnNext.setOnClickListener {
+            vavController.navigate(R.id.action_sendMoneyFragment_to_ContactSelectionFragment)
+        }
     }
 
 }
