297637dc488e4182bdf45c4439f2f5ee6fcca357
androidcodehunter <Systemuser123>
2020-03-14 00:14:48 +0600

    add some initial testing for android activity Test.

--- app/src/androidTest/java/com/memo/pay/ui/home/MainActivityTest.kt
+++ app/src/androidTest/java/com/memo/pay/ui/home/MainActivityTest.kt
@@ -2,7 +2,13 @@ package com.memo.pay.ui.home
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.LargeTest
 import com.memo.pay.data.source.AccountRepositoryImpl
 import com.memo.pay.di.repositoryModule
@@ -18,7 +24,7 @@ import org.koin.core.context.stopKoin
 import org.koin.test.AutoCloseKoinTest
 import org.koin.test.inject
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.hamcrest.Matchers
+import com.memo.pay.R
 
 
 @RunWith(AndroidJUnit4::class)
@@ -39,16 +45,28 @@ class MainActivityTest: AutoCloseKoinTest(){
 
 
     @Test
-    fun addMoneyCheck() = runBlocking{
+    fun addMoneyFlowTest() = runBlocking{
 
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
 
-        val contacts = repository.getContacts()
 
-        ViewMatchers.assertThat("savedAccount", Matchers.notNullValue())
-        ViewMatchers.assertThat("savedAccount", Matchers.notNullValue())
+        onView(withId(R.id.tvAddMoney)).perform(click())
+
+        onView(withId(R.id.etEnterAmount)).perform(replaceText("10.00"))
+
+        onView(withId(R.id.btnTopUP)).perform(click())
+
+        onView(withId(R.id.tvAccountBalance)).check(matches(isDisplayed()))
+
+        //ViewMatchers.assertThat("savedAccount", Matchers.notNullValue())
+        ///ViewMatchers.assertThat("savedAccount", Matchers.notNullValue())
 
         activityScenario.close()
+
+    }
+
+    @Test
+    fun sendMoneyFlowTest(){
 
     }
 
