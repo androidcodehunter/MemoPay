48a310e15d71d3609bd1d0d0aac144286ab081ea
Sharif <sharifur101@gmail.com>
2020-03-13 12:50:18 +0600

    add initial unit test for repository.

--- app/src/test/java/com/memo/pay/ui/home/HomeViewModelTest.kt
+++ app/src/test/java/com/memo/pay/ui/home/HomeViewModelTest.kt
@@ -3,6 +3,7 @@ package com.memo.pay.ui.home
 import com.memo.pay.data.source.FakeTestAccountRepository
 import com.memo.pay.ui.viewmodel.HomeViewModel
 import org.junit.Before
+import org.junit.Test
 
 class HomeViewModelTest{
 
@@ -16,8 +17,21 @@ class HomeViewModelTest{
        // val transaction2 = Transaction("2", "Imad", "sent", 10.00, "AED", "", Date(), "111111", "111112")
        // val transaction3 = Transaction("3", "Sharifur", "sent", 10.00, "AED", "", Date(), "111111", "111112")
       //  accountRepository.addTransactions(transaction1, transaction2, transaction3)
-        homeViewModel =
-            HomeViewModel(accountRepository)
+
+        accountRepository.addTransactions()
+
+        homeViewModel = HomeViewModel(accountRepository)
+    }
+
+    @Test
+    fun saveAccount_showAccountInformation(){
+
+    }
+
+    @Test
+    fun addMoneyTest(){
+       // homeViewModel.saveAccount()
+       /// homeViewModel.addMoney()
     }
 
 }
